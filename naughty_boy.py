371ceb9380a86c26bd138db9261e5697406583cb
Alexander Moore <ammoore@wpi.edu>
2019-11-07 20:07:30 -0500

    e

--- naughty_boy.py
+++ naughty_boy.py
@@ -1,18 +1,40 @@
-import tensorflow as tf
+# naughty naughty encoding
+# actually no just gonna learn keras here
 
-from tf.keras.models import Sequential
-from tf.keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-x_input = np.array([[1,2,3,4,5]])
-y_input = np.array([[10]])
+data = pd.read_csv('data/clean_census_income.csv')
+target = data['age']
+del data['age']
 
+scaler = MinMaxScaler()
+mms_data = scaler.fit_transform(data)
+
+x_train, x_test, y_train, y_test = train_test_split(mms_data, target, test_size = .3)
 
 model = Sequential()
-model.add(Dense(units=32, activation="tanh", input_dim=x_input.shape[1], kernel_initializer='random_normal'))
-model.add(Dense(units=1, kernel_initializer='random_normal'))
 
-model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+model.add(Dense(units = 64, activation = 'relu', input_dim = x_train.shape[1]))
+model.add(Dense(units = 1, activation = 'linear'))
 
-model.summary()
+model.compile(loss = 'MSE', optimizer = 'adam')
+
+model.fit(x_train, y_train, epochs = 5, batch_size = 32)
+
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size = 128)
+
+classes = model.predict(x_test, batch_size = 128)
+
+# note
+model.fit(x_train, y_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_val, y_val))
