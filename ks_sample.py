40181c7265bd4156f36581bd990ac649bf67e229
Alexander Moore <ammoore@wpi.edu>
2019-11-07 20:43:23 -0500

    Update ks_sample.py

--- ks_sample.py
+++ ks_sample.py
@@ -3,6 +3,7 @@
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
@@ -17,24 +18,38 @@ del data['age']
 scaler = MinMaxScaler()
 mms_data = scaler.fit_transform(data)
 
-x_train, x_test, y_train, y_test = train_test_split(mms_data, target, test_size = .3)
+x_dat, x_test, y_dat, y_test = train_test_split(mms_data, target, test_size = .2)
+x_train, x_val, y_train, y_val= train_test_split(x_dat, y_dat, test_size = .25)
 
 model = Sequential()
 
 model.add(Dense(units = 64, activation = 'relu', input_dim = x_train.shape[1]))
+model.add(Dense(units = 64, activation = 'relu'))
+model.add(Dense(units = 64, activation = 'relu'))
 model.add(Dense(units = 1, activation = 'linear'))
 
-model.compile(loss = 'MSE', optimizer = 'adam')
+atom = Adam(learning_rate = .01)
 
-model.fit(x_train, y_train, epochs = 5, batch_size = 32)
+model.compile(loss = 'MSE', optimizer = atom)
+
+history = model.fit(x_train, y_train,
+                epochs=50,
+                verbose = 0,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_val, y_val))
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
 
 loss_and_metrics = model.evaluate(x_test, y_test, batch_size = 128)
 
 classes = model.predict(x_test, batch_size = 128)
 
 # note
-model.fit(x_train, y_train,
-                epochs=50,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_val, y_val))
